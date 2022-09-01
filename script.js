c3b3596509a55bf1fb654d6735000d02b890e699
Omphyrog <framebundle@gmail.com>
2022-09-01 16:55:33 +0200

    Some changes playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3);
   if (num == 1) {
@@ -14,32 +17,38 @@ let computerSelection = getComputerChoice();
 let playerSelection = prompt("Please, choose 'Rock, paper or scissors': ");
 
 function playRound(playerSelection, computerSelection) {
-  const win = "You won!";
-  const lose = "You lose!";
-  const draw = "It's a draw!";
+  playerSelection = playerSelection.toLowerCase();
 
   if (
     (playerSelection == "rock" && computerSelection == "paper") ||
     (playerSelection == "scissors" && computerSelection == "rock") ||
     (playerSelection == "paper" && computerSelection == "scissors")
   ) {
-    return lose;
+    alert("You lose round");
+    computerScore += 1;
   } else if (
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
-    return win;
-  } else if (
-    (playerSelection == "paper" && computerSelection == "paper") ||
-    (playerSelection == "rock" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "scissors")
-  ) {
-    return draw;
+    alert("You win round");
+    playerScore += 1;
+  } else if (playerSelection === computerSelection) {
+    alert("It's a tie!");
   } else {
-    return "Wrong answer! Please, choose 'Rock, paper or scissors'";
+    alert("Wrong answer! Please, choose 'Rock, paper or scissors'");
   }
 }
 
-alert(playRound(playerSelection, computerSelection));
-// console.log(computerSelection);
+function game(playRound, playerScore, computerScore) {
+  for (let i = 0; i < 5; i++) {
+    playRound(playerSelection, computerSelection);
+    if (playerScore == 4) {
+      alert("You won the game");
+    } else if (computerScore == 4) {
+      alert("You lost the game");
+    }
+  }
+}
+
+playRound(playerSelection, computerSelection);
