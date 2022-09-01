10210532423388d18b0eaa1f2bb06f68d12d8f79
Omphyrog <framebundle@gmail.com>
2022-09-01 11:49:53 +0200

    Wrote a function that plays a single round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -11,4 +11,35 @@ function getComputerChoice() {
 
 let computerSelection = getComputerChoice();
 
-console.log(computerSelection);
+let playerSelection = prompt("Please, choose 'Rock, paper or scissors': ");
+
+function playRound(playerSelection, computerSelection) {
+  const win = "You won!";
+  const lose = "You lose!";
+  const draw = "It's a draw!";
+
+  if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "scissors" && computerSelection == "rock") ||
+    (playerSelection == "paper" && computerSelection == "scissors")
+  ) {
+    return lose;
+  } else if (
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    return win;
+  } else if (
+    (playerSelection == "paper" && computerSelection == "paper") ||
+    (playerSelection == "rock" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "scissors")
+  ) {
+    return draw;
+  } else {
+    return "Wrong answer! Please, choose 'Rock, paper or scissors'";
+  }
+}
+
+alert(playRound(playerSelection, computerSelection));
+// console.log(computerSelection);
