cda45c70fa8f59c7672dfe12d1a4cda5c507b6e1
Omphyrog <framebundle@gmail.com>
2022-09-26 16:01:53 +0200

    First attempt at UI

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let playerScore = 0;
-let computerScore = 0;
+let playerSelection;
+let computerSelection;
+
+const selectionBtns = document.querySelectorAll("[data-selection]");
+
+selectionBtns.forEach((selectionBtn) => {
+  selectionBtn.addEventListener("click", (e) => {
+    const selectionName = selectionBtn.dataset.selection;
+    makeSelection(selectionName);
+    console.log(playerSelection);
+  });
+});
+
+function makeSelection(selection) {
+  return selection;
+}
 
 function getComputerChoice() {
   let num = Math.floor(Math.random() * 3);
@@ -11,79 +25,49 @@ function getComputerChoice() {
     return "paper";
   }
 }
-
-let computerSelection;
-
-let playerSelection;
+let score = 0;
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-
+  computerSelection = getComputerChoice();
+  playerSelection = makeSelection();
   if (
     (playerSelection == "rock" && computerSelection == "paper") ||
     (playerSelection == "scissors" && computerSelection == "rock") ||
     (playerSelection == "paper" && computerSelection == "scissors")
   ) {
-    alert(
-      "You lose round " +
-        "Computer: " +
-        computerScore +
-        " " +
-        "Player: " +
-        playerScore
-    );
-    computerScore += 1;
+    console.log("You lose");
+    score--;
   } else if (
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
-    alert(
-      "You win round" +
-        " " +
-        "Computer: " +
-        computerScore +
-        " " +
-        "Player: " +
-        playerScore
-    );
-    playerScore += 1;
-  } else if (playerSelection === computerSelection) {
-    alert(
-      "It's a tie!" +
-        " " +
-        "Computer: " +
-        computerScore +
-        " " +
-        "Player: " +
-        playerScore
-    );
-  } else {
-    alert(
-      "Wrong answer! Please, choose 'Rock, paper or scissors'" +
-        " " +
-        "Computer: " +
-        computerScore +
-        " " +
-        "Player: " +
-        playerScore
-    );
+    console.log("You win");
+    score++;
+  } else if (
+    (playerSelection == "paper" && computerSelection == "paper") ||
+    (playerSelection == "scissors" && computerSelection == "scissors") ||
+    (playerSelection == "rock" && computerSelection == "rock")
+  ) {
+    console.log("Its a draw");
+    score += 0;
   }
+  return score;
 }
 
-function game(playRound) {
-  for (let i = 0; i < Infinity; i++) {
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Please, choose 'Rock, paper or scissors': ");
-    playRound(playerSelection, computerSelection);
-    if (playerScore == 5) {
-      alert("You won the game");
-      break;
-    } else if (computerScore == 5) {
-      alert("Computer won");
-      break;
-    }
-  }
-}
+// function game(playRound) {
+//   for (let i = 0; i < Infinity; i++) {
+//     // computerSelection = getComputerChoice();
+//     // playerSelection = prompt("Please, choose 'Rock, paper or scissors': ");
+//     playRound(playerSelection, computerSelection);
+//     if (score === 5) {
+//       alert("You won the game");
+//       break;
+//     } else if (score < 0) {
+//       alert("Computer won");
+//       break;
+//     }
+//   }
+// }
 
-game(playRound);
+// game(playRound);
